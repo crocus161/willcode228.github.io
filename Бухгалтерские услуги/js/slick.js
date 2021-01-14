36fc426829318eaa6be6ab25006598a440b61004
willcode228 <55146834+willcode228@users.noreply.github.com>
2021-01-14 14:50:31 +0200

    Add files via upload

---
+++ Бухгалтерские услуги/js/slick.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.header-slider').slick({
+        dots: true,
+        arrows: false,
+    });
+  });
+  $(document).ready(function(){
+    $('.work-slider').slick({
+        dots: true,
+        arrows: false,
+    });
+  });
+  $(document).ready(function(){
+    $('.review-slick').slick({
+        dots: true,
+        arrows: false,
+    });
+  });
