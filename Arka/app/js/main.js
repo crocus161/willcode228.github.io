49613c6d853abdbb190be74938b6796cc49638d6
willcode228 <crocus161@gmail.com>
2021-03-07 00:43:03 +0200

    Add base interface of the site

---
+++ Arka/app/js/main.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', () => {
+    const burger = document.querySelector('.nav__burger-btn'),
+          menu = document.querySelector('.nav__menu');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger-active');
+        menu.classList.toggle('nav__menu-active');
+    });
+
+    menu.addEventListener('click', (e) => {
+        setTimeout(() => {
+            if(e.target.classList.contains('nav__link')){
+                menu.classList.remove('nav__menu-active');
+            }
+        }, 300);
+    });
+    
+});
