9dc4f01976feea120e825fb8a32d80d2eecb50a3
willcode228 <crocus161@gmail.com>
2021-03-08 14:43:22 +0200

    frilance project

---
+++ Поправить слайдеры/dist/js/script.js
@@ -0,0 +1,12 @@
+
+  $('.examples__list_3').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+  $('.introduction__list_3').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
+  });
+      
