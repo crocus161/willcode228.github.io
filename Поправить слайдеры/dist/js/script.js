d898d4548e979b5691233bc78de722407b3df596
willcode228 <crocus161@gmail.com>
2021-03-08 23:58:15 +0200

    Add functionality

--- Поправить слайдеры/dist/js/script.js
+++ Поправить слайдеры/dist/js/script.js
@@ -1,4 +1,24 @@
+$(document).ready(function() {
 
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+  });
+  //E-mail Ajax Send
+
+  //slick slider settings
   $('.examples__list_3').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -9,4 +29,8 @@
     slidesToScroll: 1,
     arrows: false
   });
+  //slick slider settings
+
+
+});
       
