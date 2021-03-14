0fa56bb2f2ee614183e0847dd7cb24a6a41e112f
willcode228 <crocus161@gmail.com>
2021-03-14 14:53:27 +0200

    The end of the project

--- Blank/app/js/main.js
+++ Blank/app/js/main.js
@@ -2,16 +2,22 @@ window.addEventListener('load', () => {
 
     //preloader settings
 
-    // const preloader = document.querySelector('.preloader');
-    // setTimeout(() => {
-    //     preloader.style.opacity = 0;
-    //     setTimeout(() => {
-    //         preloader.style.display = 'none';
-    //     }, 1000);
-    // }, 3000);
+    const preloader = document.querySelector('.preloader');
+    setTimeout(() => {
+        preloader.style.opacity = 0;
+        setTimeout(() => {
+            preloader.style.display = 'none';
+        }, 1000);
+    }, 3000);
 
     //preloader settings
 
+    //active navigation on scroll
+    const nav = document.querySelector('.nav');
+    window.addEventListener('scroll', () => {
+        nav.classList.toggle('nav-active', scrollY > 0);
+    });
+    //active navigation on scroll
 
 
     //navigation settings
@@ -34,29 +40,29 @@ window.addEventListener('load', () => {
     //navigation settings
 
     //smooth scroll
-    // let linkNav = document.querySelectorAll('[href^="#"]'),
-    //     V = 0.4;
-    // for (let i = 0; i < linkNav.length; i++) {
-    //     linkNav[i].addEventListener('click', function (e) {
-    //         e.preventDefault();
-    //         let w = window.pageYOffset,
-    //             hash = this.href.replace(/[^#]*(.*)/, '$1');
-    //         t = document.querySelector(hash).getBoundingClientRect().top,
-    //             start = null;
-    //         requestAnimationFrame(step);
-    //         function step(time) {
-    //             if (start === null) start = time;
-    //             let progress = time - start,
-    //                 r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
-    //             window.scrollTo(0, r);
-    //             if (r != w + t) {
-    //                 requestAnimationFrame(step);
-    //             } else {
-    //                 location.hash = hash;
-    //             }
-    //         }
-    //     }, false);
-    // }
+    let linkNav = document.querySelectorAll('[href^="#"]'),
+        V = 0.4;
+    for (let i = 0; i < linkNav.length; i++) {
+        linkNav[i].addEventListener('click', function (e) {
+            e.preventDefault();
+            let w = window.pageYOffset,
+                hash = this.href.replace(/[^#]*(.*)/, '$1');
+            t = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+            requestAnimationFrame(step);
+            function step(time) {
+                if (start === null) start = time;
+                let progress = time - start,
+                    r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+                window.scrollTo(0, r);
+                if (r != w + t) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        }, false);
+    }
     //smooth scroll
 
 });
