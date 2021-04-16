cf3a81be9c6fc6925ba53b76e9629c9021823bcf
willcode228 <crocus161@gmail.com>
2021-04-17 01:01:40 +0300

    Create font-size grow animation

--- Отношения/app/js/main.js
+++ Отношения/app/js/main.js
@@ -11,4 +11,33 @@ window.addEventListener('load', () => {
     nav_close.addEventListener('click', () => {
         nav_list.classList.remove('nav__list-active');
     });
+
+
+
+    let width = window.innerWidth,
+        size_grow = 0;
+    width <= 576 ? size_grow = 12 : size_grow = 50;
+
+    const effect_title = $('.scroll__effect-text');
+    const default_size = parseInt(window.getComputedStyle(effect_title).fontSize);
+    let default_scroll = 0;
+    window.addEventListener('scroll', () => {
+        let size = parseInt(window.getComputedStyle(effect_title).fontSize);
+
+        if(default_scroll < scrollY && 
+            size < (default_size + size_grow) && 
+            effect_title.getBoundingClientRect().top < size_grow){
+
+            effect_title.style.fontSize = `${size + 2}px`;
+
+        }else if(default_scroll > scrollY && 
+                size > default_size && 
+                effect_title.getBoundingClientRect().top > -200){
+
+            effect_title.style.fontSize = `${size - 2}px`;
+
+        } 
+        default_scroll = scrollY;
+
+    });
 });
